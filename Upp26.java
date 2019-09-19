18dba3a4b422126c00b1cf9dcb1d5d0f2f6aac9a
Rasmus Eriksson Gillberg <rasmus.gillberg@learnet.se>
2019-09-19 11:16:27 +0200

    Uppgifter 2.7 och 2.8

---
+++ Upp26.java
@@ -0,0 +1,17 @@
+/*
+ Uppgift2.6
+ Skapat 2019-09-19
+ Rasmus Gillberg
+*/
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Upp26 {
+	public static void main(String[] args) {
+		char c1 = '\u00D8';
+		char c2 = '\u00F8';
+	
+		JOptionPane.showMessageDialog(null, c1 + "\n" + c2);
+	}
+}
---
+++ Upp27.java
@@ -0,0 +1,17 @@
+/*
+ Uppgift2.7
+ Skapat 2019-09-19
+ Rasmus Gillberg
+*/
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Upp27 {
+	public static void main(String[] args) {
+		char c1 = '\u00D8';
+		char c2 = '\u00F8';
+		char c3 = '\u03C0';
+		JOptionPane.showMessageDialog(null, c1 + "\n" + c2 + "\n" + c3);
+	}
+}
